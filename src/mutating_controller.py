d8c56928bbb828b1cb09f496650b9d95124181a9
Jack Alder <jack.alder@mongodb.com>
2023-02-23 11:10:48 -0500

    hardening against invalid object patches on appdb

--- src/mutating_controller.py
+++ src/mutating_controller.py
@@ -19,13 +19,16 @@ def mutator():
               if resource["request"]["object"]["metadata"]["labels"]["controller"] == "mongodb-enterprise-operator":
                 ## If so, set pod antiAffinity for requiredDuringSchedulingIgnoreDuringExecution to value "pod-anti-affinity: affinity-0"
                 if "pod-anti-affinity" in resource["request"]["object"]["metadata"]["labels"]:
-                  antiAffinityRule = resource["request"]["object"]["metadata"]["labels"]["pod-anti-affinity"]
-                  message = "Adjusted anti-affinity pod rules for "+antiAffinityRule
-                  requestId = resource["request"]["uid"]
-                  patch = jsonpatch.JsonPatch([{"op": "add", "path": "/spec/affinity/podAntiAffinity/requiredDuringSchedulingIgnoredDuringExecution", "value": [{"labelSelector":{"matchExpressions":[{"key":"pod-anti-affinity","operator":"In","values":[antiAffinityRule]}]},"topologyKey":"kubernetes.io/hostname"}] }])
-                  encoded_patch = base64.b64encode(patch.to_string().encode("utf-8")).decode("utf-8")
-                  response = jsonify({"apiVersion": "admission.k8s.io/v1","kind": "AdmissionReview","response":{"uid": requestId,"allowed": True, "status": {"message":message},"patchType":"JSONPatch","patch": encoded_patch}})
-                  return response
+                  ## The Ops Manager applicationDatabase sts has labels but no affinity rules, ignore these resources
+                  if "affinity" in resource["request"]["object"]["spec"]:
+                    if "podAntiAffinity" in resource["request"]["object"]["spec"]["affinity"]:
+                      antiAffinityRule = resource["request"]["object"]["metadata"]["labels"]["pod-anti-affinity"]
+                      message = "Adjusted anti-affinity pod rules for "+antiAffinityRule
+                      requestId = resource["request"]["uid"]
+                      patch = jsonpatch.JsonPatch([{"op": "add", "path": "/spec/affinity/podAntiAffinity/requiredDuringSchedulingIgnoredDuringExecution", "value": [{"labelSelector":{"matchExpressions":[{"key":"pod-anti-affinity","operator":"In","values":[antiAffinityRule]}]},"topologyKey":"kubernetes.io/hostname"}] }])
+                      encoded_patch = base64.b64encode(patch.to_string().encode("utf-8")).decode("utf-8")
+                      response = jsonify({"apiVersion": "admission.k8s.io/v1","kind": "AdmissionReview","response":{"uid": requestId,"allowed": True, "status": {"message":message},"patchType":"JSONPatch","patch": encoded_patch}})
+                      return response
     return ignore
   else:
     ## No request received
